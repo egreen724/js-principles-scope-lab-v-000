794090267cbbfa77708a82850f2c07f39768b11e
Erin Green <erin.y.green@gmail.com>
2019-05-02 20:07:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+
+var customerName = "Erin"; 
+
+function upperCaseCustomerName(name) {
+  let firstLetter = customerName.charAt(0).toUpperCase()
+  let restOfName = customerName.slice(1)
+
+  return firstLetter + restOfName;
+}
